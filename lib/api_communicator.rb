0cf336936ef8e42489257402f570e3f599a8e8cb
francosta <francisco@fcosta.pt>
2019-04-02 11:26:46 +0100

    addded pry

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,6 +4,7 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
+  binding.pry
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
