6385a07d8126b30217053939fcf0c3d644773d54
Ay Has <ahashim983@yahoo.com>
2019-04-02 15:24:44 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,6 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+#character = get_character_from_user
+get_character_movies_from_api("Luke Skywalker")
+show_character_movies("Luke Skywalker")
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,32 +2,55 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  to_return = []
+  i = 0
+  while i < response_hash['results'].length
+    if response_hash["results"][i]['name']== character_name
+      to_return.push(response_hash['results'][i]['films'])
+      break
+      #puts response_hash['results'][i]['films']
+    end
+    i = i+1
+  end
+  films_urls = to_return.flatten
+  #puts films_urls
+  values_to_return = []
+  j=0
+  while j < films_urls.length
+    film_string = RestClient.get(films_urls[j])
+    film_hash = JSON.parse(film_string)
+    values_to_return.push(film_hash)
+    j = j+1
+  end
+return values_to_return
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  i = 0
+  while i < films.length
+    each_hash = films[i]
+    each_hash.each do |key,data|
+      if key == "title"
+        print data
+        break
+      end
+
+    end
+  end
 end
+
+
+  # some iteration magic and puts out the movies in a nice list
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
 
-## BONUS
-
-# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
-# can you split it up into helper methods?
+puts show_character_movies("Luke Skywalker")
