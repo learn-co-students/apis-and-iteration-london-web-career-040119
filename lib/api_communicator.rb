216ed2c69e3059c349aadeaec7b3c2d785b305e9
Danny Wakeling <45290034+Dwake5@users.noreply.github.com>
2019-04-05 14:17:52 +0100

    updated to get green light on learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -5,7 +5,7 @@ require 'pry'
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
+  response_hash = JSON.parse(response_string) 
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
